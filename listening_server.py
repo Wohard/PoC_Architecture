5c54ac4c3514906181d8ed9868e7e937c4f62a6e
Wohard <bellaoui.wohard@gmail.com>
2016-01-14 18:50:55 +0100

    Create listening_server.py

---
+++ listening_server.py
@@ -0,0 +1,49 @@
+import admin_controller
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+
+# Restrict to a particular path.
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+# Create server
+server = SimpleXMLRPCServer(("localhost", 8769),
+                            requestHandler=RequestHandler)
+server.register_introspection_functions()
+
+# Register pow() function; this will use the value of
+# pow.__name__ as the name, which is just 'pow'.
+server.register_function(pow)
+
+# Register a function under a different name
+def adder_function(x,y):
+    return x + y
+server.register_function(adder_function, 'add')
+
+# Register an instance; all the methods of the instance are
+# published as XML-RPC methods (in this case, just 'div').
+class MyFuncs:
+    def div(self, x, y):
+        return x // y
+
+server.register_instance(MyFuncs())
+
+#
+# Resister the function to be called "admin_controller.py"
+
+server.register_function(admin_controller, 'probe')
+
+# Run the server's main loop
+server.serve_forever()
+
+"""
+import xmlrpclib
+
+s = xmlrpclib.ServerProxy('http://localhost:8000')
+print s.pow(2,3)  # Returns 2**3 = 8
+print s.add(2,3)  # Returns 5
+print s.div(5,2)  # Returns 5//2 = 2
+
+# Print list of available methods
+
+"""
