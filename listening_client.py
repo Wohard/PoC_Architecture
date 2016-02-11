b202d9a46cff705d229602664c3161105b268726
Wohard <bellaoui.wohard@gmail.com>
2016-02-11 13:44:59 +0100

    Update listening_client.py

--- listening_client.py
+++ listening_client.py
@@ -1,9 +1,10 @@
+
 import xmlrpclib
 
-s = xmlrpclib.ServerProxy('http://localhost:8769')
-print s.pow(2,3)  # Returns 2**3 = 8
-print s.add(2,3)  # Returns 5
-print s.div(5,2)  # Returns 5//2 = 2
-
+s = xmlrpclib.ServerProxy('http://localhost:48769') # the address of the server 
+#print s.pow(2,3)  # Returns 2**3 = 8
+#print s.add(2,3)  # Returns 5
+#print s.div(5,2)  # Returns 5//2 = 2
+s.probe("msg_ok", "10.0.1.7")
 # Print list of available methods
-print s.system.listMethods()
+#print s.system.listMethods()
